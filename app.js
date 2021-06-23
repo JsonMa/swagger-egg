61d8fe8c832cd1ae62a73cedf4faebca1d72cf22
jsonma <775087367@qq.com>
2021-06-23 16:06:53 +0800

    fix(config): remove swagger ui config

--- app.js
+++ app.js
@@ -2,26 +2,25 @@
 
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 const Swagger = require('./lib/swagger');
+
 class AppBootHook {
   constructor(app) {
     this.app = app;
   }
 
   configWillLoad() {
-    const { swaggerEgg } = this.app.config;
-    if (swaggerEgg.ui && swaggerEgg.ui.enable) {
-      let staticDirs = this.app.config.static.dir;
-      // CAUTION: index.html under 'app/public' directory will be overwrite by 'swagger-ui-dist'
-      if (Array.isArray(staticDirs)) staticDirs.unshift(pathToSwaggerUi);
-      else staticDirs = [ pathToSwaggerUi, staticDirs ];
-      this.app.config.static.dir = staticDirs;
-    }
+    let staticDirs = this.app.config.static.dir;
+    // CAUTION: index.html under 'app/public' directory will be overwrite by 'swagger-ui-dist'
+    if (Array.isArray(staticDirs)) staticDirs.unshift(pathToSwaggerUi);
+    else staticDirs = [ pathToSwaggerUi, staticDirs ];
+    this.app.config.static.dir = staticDirs;
   }
 
   didLoad() {
     try {
       new Swagger(this.app, pathToSwaggerUi);
     } catch (error) {
+      error.message = `[swagger-egg]: ${error.message}`;
       throw error;
     }
   }
