da60e343631378f23239784787dee4d2c2a26edb
jsonma <775087367@qq.com>
2022-01-06 09:51:53 +0800

    fix(router): :bug: fix router name bug

--- test/fixtures/apps/swagger-egg-router-test/app/router/admin.js
+++ test/fixtures/apps/swagger-egg-router-test/app/router/admin.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports = app => {
+module.exports = (app) => {
   const { router, controller } = app;
 
   // Common style router
   router.get('/admin', controller.admin.index);
-  router.get('/admin/new', controller.admin.new);
-  router.get('/admin/:id', controller.admin.show);
+  router.get('newAdmin', '/admin/new', controller.admin.new);
+  router.get('newShow', '/admin/:id', controller.admin.show);
   router.get('/admin/:id/edit', controller.admin.edit);
   router.post('/admin', controller.admin.create);
   router.put('/admin/:id', controller.admin.update);
